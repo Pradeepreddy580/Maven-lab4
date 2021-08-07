935592f6058f3705987bad83dcab4ef7a33547e3
Pradeep <74000008+Pradeepreddy580@users.noreply.github.com>
2021-08-07 10:17:32 +0530

    Update Greeter.java

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -1,7 +1,7 @@
 package hello;
 
-public class Greeter {
- // public String sayHello() {
+//public class Greeter {
+ public String sayHello() {
     return "Hello world----- pavan is your Trainer ! This is for CICD demo ,Need Bye string to pass the test so Bye";
   }
 }
